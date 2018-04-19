1e92c9ac9ef14daf9b49770a9a34e479c9e188b1
codelapp <info@codelapp.com>
2018-04-19 17:52:24 +0200

    Primer commit

--- Sounds/AppDelegate.swift
+++ Sounds/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Sounds
-//
-//  Created by Jorge Maldonado Borbón on 06/11/17.
-//  Copyright © 2017 Jorge Maldonado Borbón. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
--- Sounds/ViewController.swift
+++ Sounds/ViewController.swift
@@ -1,25 +1,67 @@
-//
-//  ViewController.swift
-//  Sounds
-//
-//  Created by Jorge Maldonado Borbón on 06/11/17.
-//  Copyright © 2017 Jorge Maldonado Borbón. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imagen: UIImageView!
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+     
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
+    @IBAction func botones(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 1:
+            imagen.image = UIImage(named: "perro")
+            sonidos(sonido: "perro")
+        case 2:
+            imagen.image = UIImage(named: "gato")
+            sonidos(sonido: "gato")
+        case 3:
+            imagen.image = UIImage(named: "elefante")
+            sonidos(sonido: "elefante")
+        case 4:
+            imagen.image = UIImage(named: "avion")
+            sonidos(sonido: "avion")
+        case 5:
+            imagen.image = UIImage(named: "carro")
+            sonidos(sonido: "carro")
+        case 6:
+            imagen.image = UIImage(named: "moto")
+            sonidos(sonido: "moto")
+        case 7:
+            imagen.image = UIImage(named: "tren")
+            sonidos(sonido: "train")
+        case 8:
+            imagen.image = UIImage(named: "gallo")
+            sonidos(sonido: "Gallo")
+        case 9:
+            imagen.image = UIImage(named: "pato")
+            sonidos(sonido: "pato")
+        default:
+            print("no hay mas")
+        }
+        
+    }
+    
+    func sonidos(sonido: String){
+        do {
+            let audioPath = Bundle.main.path(forResource: sonido, ofType: "mp3")
+            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+            player.play()
+        } catch let error as NSError {
+            print("hubo un error", error)
+        }
+    }
+    
 
 }
 
